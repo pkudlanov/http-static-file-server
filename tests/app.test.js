adaf29fd7d0dac24f79e1b59c7684351b1c823f1
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-06-28 14:07:49 -0700

    wrote test if we get index.html when we request it

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,8 +1,19 @@
+const request = require('supertest');
+const app = require('../lib/app');
 const { getFilePath } = require('../lib/utils/filePath');
 
 describe('app static server', () => {
     it('get a file path from url path', () => {
-        const filePath = getFilePath('./index.html');
+        const filePath = getFilePath('/index.html');
         expect(filePath).toEqual('./public/index.html');
+    });
+
+    it('gets an index.html if we request it', () => {
+        return request(app)
+            .get('/index.html')
+            .then(res => {
+                expect(res.status).toEqual(200);
+                expect(res.text).toEqual(expect.stringContaining('<h1>Greetngs to all Far and Near!!!</h1>'));
+            });
     });
 });
