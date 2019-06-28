5a22eff76ad36be2220536a88b56a1df51400e03
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-06-28 16:56:12 -0700

    added some dom elements to the html page

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+const root = document.getElementById('root');
+
+const heading = document.createElement('h2');
+heading.textContent = 'The Great Dog Space Travelers';
+root.appendChild(heading);
+
+const paragraph = document.createElement('p');
+paragraph.textContent = 'I have you a tale of some great animals that walked this Earth. Belka and Strelka were there names. They were a great and brave kind of dog.';
+root.appendChild(paragraph);
+
